e0fc70dc43e0595b135d886ac49a0ce2e8bcf05a
Vladimir Borisov <1mt-2@mail.ru>
2022-08-25 12:07:27 +0300

    added editable span

--- src/p2-homeworks/h6/HW6.tsx
+++ src/p2-homeworks/h6/HW6.tsx
@@ -1,7 +1,8 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import SuperEditableSpan from './common/c4-SuperEditableSpan/SuperEditableSpan'
 import SuperButton from '../h4/common/c2-SuperButton/SuperButton'
 import {restoreState, saveState} from './localStorage/localStorage'
+import s from './HW6.module.css'
 
 function HW6() {
     const [value, setValue] = useState<string>('')
@@ -9,26 +10,33 @@ function HW6() {
     const save = () => {
         saveState<string>('editable-span-value', value)
     }
+
     const restore = () => {
-        // setValue()
+        setValue(restoreState('editable-span-value', value))
     }
+
 
     return (
         <div>
             <hr/>
-            homeworks 6
+            <div>homeworks 6</div>
+
 
             {/*should work (должно работать)*/}
-            <div>
+            <div className={s.span}>
+                <img
+                    src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQw2bfTl_KQgJafLj4Vs72dUyA_Q_7xnrcrUA&usqp=CAU'
+                    className={s.pic}/>
                 <SuperEditableSpan
                     value={value}
                     onChangeText={setValue}
                     spanProps={{children: value ? undefined : 'enter text...'}}
                 />
             </div>
-            <SuperButton onClick={save}>save</SuperButton>
-            <SuperButton onClick={restore}>restore</SuperButton>
-
+            <div>
+                <SuperButton onClick={save}>save</SuperButton>
+                <SuperButton onClick={restore}>restore</SuperButton>
+            </div>
             <hr/>
             {/*для личного творчества, могу проверить*/}
             {/*<AlternativeSuperEditableSpan/>*/}
