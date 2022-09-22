f2bde09ede1efc17dd1c71cdebd50b8ef7421cf6
Vladimir Borisov <1mt-2@mail.ru>
2022-09-22 13:13:33 +0300

    corrected styles

--- src/p2-homeworks/h2/Affairs.tsx
+++ src/p2-homeworks/h2/Affairs.tsx
@@ -5,8 +5,8 @@ import s from './Affairs.module.css'
 
 type AffairsPropsType = { // need to fix any
     data: Array<AffairType>
-    setFilter: (filter: FilterType)=>void
-    deleteAffairCallback: (_id: number)=>void
+    setFilter: (filter: FilterType) => void
+    deleteAffairCallback: (_id: number) => void
 }
 
 function Affairs(props: AffairsPropsType) {
@@ -18,20 +18,28 @@ function Affairs(props: AffairsPropsType) {
         />
     ))
 
-    const setAll = () => {props.setFilter('all')} // need to fix
-    const setHigh = () => {props.setFilter('high')}
-    const setMiddle = () => {props.setFilter('middle')}
-    const setLow = () => {props.setFilter('low')}
+    const setAll = () => {
+        props.setFilter('all')
+    } // need to fix
+    const setHigh = () => {
+        props.setFilter('high')
+    }
+    const setMiddle = () => {
+        props.setFilter('middle')
+    }
+    const setLow = () => {
+        props.setFilter('low')
+    }
 
     return (
-        <div className={s.filterButtonBlock}>
-
-            {mappedAffairs}
-
-            <button onClick={setAll} className={s.filterButton}>All</button>
-            <button onClick={setHigh} className={s.filterButton}>High</button>
-            <button onClick={setMiddle} className={s.filterButton}>Middle</button>
-            <button onClick={setLow} className={s.filterButton}>Low</button>
+        <div className={s.main}>
+            <div className={s.table}>{mappedAffairs}</div>
+            <div className={s.filterButtonBlock}>
+                <button onClick={setAll} className={s.filterButton}>All</button>
+                <button onClick={setHigh} className={s.filterButton}>High</button>
+                <button onClick={setMiddle} className={s.filterButton}>Middle</button>
+                <button onClick={setLow} className={s.filterButton}>Low</button>
+            </div>
         </div>
     )
 }
